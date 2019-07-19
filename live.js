da8dd82e7f276a67dc9189c2e803060b1d15b9e6
Trung <TrungNV984@gmail.com>
2019-07-19 11:57:08 +0700

    update log to ubuntu

--- live.js
+++ live.js
@@ -1,4 +1,11 @@
 const runner = require('child_process');
-runner.exec('node play.js >> outputs\\1.txt', function (err, response, stderr) {
-    process.abort();
-});
+var isWin = process.platform === 'win32';
+if (isWin) {
+    runner.exec('node play.js >> outputs\\1.txt', function (err, response, stderr) {
+        process.abort();
+    });
+} else {
+    runner.exec('node play.js >> outputs\\`date +"%Y-%m-%d"`.txt', function (err, response, stderr) {
+        process.abort();
+    });
+}
